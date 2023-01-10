19de1a54cff993047fd0d13a7a828dfe75f9166f
TangHang123 <1425735414@qq.com>
2023-01-10 17:19:57 +0800

    -

--- app/renderer/src/main/src/pages/MainOperator.tsx
+++ app/renderer/src/main/src/pages/MainOperator.tsx
@@ -154,6 +154,8 @@ export interface MenuItem {
 export interface MenuItemGroup {
     Group: string
     Items: MenuItem[]
+    MenuSort: number
+    Mode: string
 }
 
 interface PluginMenuItem {
@@ -1203,6 +1205,13 @@ const Main: React.FC<MainProp> = React.memo((props) => {
             />
         )
     }
+    const onRouteMenuSelect = useMemoizedFn((key: string) => {
+        if (!key || key === "undefined") {
+            failed("不存在")
+            return
+        }
+        menuAddPage(key as Route)
+    })
     return (
         <Layout className='yakit-main-layout'>
             <AutoSpin spinning={loading}>
@@ -1410,7 +1419,7 @@ const Main: React.FC<MainProp> = React.memo((props) => {
                         <HeardMenu
                             routeMenuData={(routeMenuData || []).filter((e) => !e.hidden)}
                             menuItemGroup={menuItems}
-                            onRouteMenuSelect={menuAddPage}
+                            onRouteMenuSelect={onRouteMenuSelect}
                         />
                         <Content
                             style={{
